b0fac052380aabd7cc199a09a0ff75230c25fcc9
yuri <yuri.shangaraev@gmail.com>
2023-04-10 01:49:04 +0500

    correct

--- app/db/handlers.py
+++ app/db/handlers.py
@@ -11,28 +11,33 @@ async def data_handler(dt_from, dt_upto, group_type):
     result["labels"] = time_range(dt_from, dt_upto, group_type, result["labels"])
     result["labels"].append(datetime.isoformat(dt_upto))
 
-    pipeline = [{'$match': {'dt': {'$gte': dt_from, '$lt': dt_upto}}},
+    pipeline = [{'$match': {'dt': {'$lte': dt_upto, '$gte': dt_from}}},
                 {'$sort': {'dt': 1}}]
 
     cursor = collection.aggregate(pipeline)
     documents = await cursor.to_list(None)
+    last_date = documents[0]["dt"]
 
     if documents:
         for date in result["labels"][1:]:
             val = 0
             count = 0
-            for document in documents:
-                if document["dt"] < datetime.fromisoformat(date):
-                    val += document["value"]
-                    count += 1
-                else:
-                    documents = documents[count:]
-                    result["dataset"].append(val)
-                    break
+
+            # проверяем, чтобы дата всегда была больше предыдущей
+            if documents[0]["dt"] >= last_date:
+                for document in documents:
+                    if document["dt"] < datetime.fromisoformat(date) or document["dt"] == datetime.fromisoformat(result["labels"][-1]):
+                        count += 1
+                        val += document["value"]
+                        if last_date < document["dt"]:
+                            last_date = document["dt"]
+                    else:
+                        documents = documents[count:]
+                        break
+            result["dataset"].append(val)
     else:
         return "На указанные даты данных нет"
 
-    result["dataset"].append(val)
     result["labels"].pop(-1)
-
-    return str(result)
+    str_result = str(result).replace("'", '"')
+    return str_result
